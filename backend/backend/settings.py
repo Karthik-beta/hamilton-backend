ae056a2711044ac94ccf63a30202dfad9c2574ca
Karthik-beta <karthikpar@protonmail.com>
2023-12-07 12:31:09 +0530

    1.0.3
    Changed to Postgres Image

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,25 +86,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # SQLite Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'magnum',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'magnum',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'db', # This is the name of the PostgreSQL service in the Docker Compose file
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
