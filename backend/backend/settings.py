06d002694222ba25fa3c4bbcf49c278d05079727
Karthik-beta <karthikpar@protonmail.com>
2023-12-26 13:44:19 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'password123',
         'HOST': 'localhost',
+        'HOST': '192.168.0.56',
         # 'HOST': 'db', # This is the name of the PostgreSQL service in the Docker Compose file
         'PORT': '5432',
     }
