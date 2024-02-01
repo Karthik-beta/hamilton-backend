884a1044993685941925161ffe25af71c3bc93d9
Karthik-beta <karthikpar@protonmail.com>
2024-02-01 15:29:02 +0530

    Update cron2.py

--- cron2.py
+++ cron2.py
@@ -1,23 +1,17 @@
 import subprocess
 
-def execute_command_in_docker(image_name, command):
+def run_docker_command():
     try:
-        # Command to enter Docker bash shell and execute the provided command
-        docker_command = f"docker run -it {image_name} /bin/bash -c '{command}'"
+        # Command to run
+        command = ["sudo", "docker", "exec", "-it", "sael-backend-backend-1", "python", "manage.py", "assemblyline_andon"]
 
-        # Execute the Docker command using subprocess
-        subprocess.run(docker_command, shell=True, check=True)
+        # Run the command
+        subprocess.run(command, check=True)
 
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    except Exception as ex:
+        print(f"An unexpected error occurred: {ex}")
 
-# Replace 'your_docker_image_name' with the actual name of your Docker image
-docker_image_name = 'sael-backend-backend-1'
-
-# Command to be executed within the Docker bash shell
-docker_command_to_execute = "python manage.py assemblyline_andon"
-
-# Call the function with the provided image name and command
-execute_command_in_docker(docker_image_name, docker_command_to_execute)
+if __name__ == "__main__":
+    run_docker_command()
